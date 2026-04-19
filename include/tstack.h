f0b42bdeab473c3c121887120c63480a5285ba56
maksimkk6 <konnovmaks@bk.ru>
2026-04-19 19:42:32 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,36 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+
 template<typename T, int size>
 class TStack {
-private:
-    T arr[size];
-    int top_idx;    
-public:
-    TStack() : top_idx(-1) {}
-    
-    void push(const T& value) {
-        if (top_idx < size - 1) {
-            arr[++top_idx] = value;
-        }
-    }    
-    T pop() {
-        if (top_idx >= 0) {
-            return arr[top_idx--];
-        }
-        return T();
-    }    
-    T top() const {
-        if (top_idx >= 0) {
-            return arr[top_idx];
-        }
-        return T();
-    }    
-    bool isEmpty() const {
-        return top_idx == -1;
+ private:
+  T arr[size];
+  int top_idx;
+
+ public:
+  TStack() : top_idx(-1) {}
+
+  void push(const T& value) {
+    if (top_idx < size - 1) {
+      arr[++top_idx] = value;
     }
-    bool isFull() const {
-        return top_idx == size - 1;
+  }
+
+  T pop() {
+    if (top_idx >= 0) {
+      return arr[top_idx--];
     }
+    return T();
+  }
+
+  T top() const {
+    if (top_idx >= 0) {
+      return arr[top_idx];
+    }
+    return T();
+  }
+
+  bool isEmpty() const {
+    return top_idx == -1;
+  }
+
+  bool isFull() const {
+    return top_idx == size - 1;
+  }
 };
+
 #endif  // INCLUDE_TSTACK_H_
